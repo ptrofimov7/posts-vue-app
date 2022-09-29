62d330a61f0bddc8116ff221ee576c0b6358f3fe
Peter Trofimov <ptrofimov7@gmail.com>
2022-09-29 11:07:02 +0300

    inits vue js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import components from '@/components/UI'
+import directives from '@/directives'
+import VIntersection from '@/directives/VIntersection'
+import router from '@/router/router'
+import store from '@/store'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+components.forEach(component => app.component(component.name, component))
+directives.forEach(directive => app.directive(directive.name, directive))
+app.use(router)
+.use(store)
+.mount('#app')
